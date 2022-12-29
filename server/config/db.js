be7bb1f96ebb9092aab8ac1f97b51242a202f372
Michael Chop <007micle007@gmail.com>
2022-12-29 20:54:26 +0300

    Added some mongoose models

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    const connection = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB connected: ${connection.connection.host}`.cyan.underline.bold)
+}
+
+module.exports = connectDB
--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const colors = require('colors')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+//Connect to database
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
