a0a73d04803f29339d9a42182254cafba45347a5
Michael Chop <007micle007@gmail.com>
2022-12-30 22:40:45 +0300

    Added fetch and display clients functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
+import Clients from "./components/Clients";
 
 const client = new ApolloClient({
     uri: 'http://localhost:5000/graphql',
@@ -12,7 +13,7 @@ function App() {
             <ApolloProvider client={client}>
                 <Header/>
                 <div className="container">
-                    <h1>Hello</h1>
+                    <Clients />
                 </div>
             </ApolloProvider>
         </>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
@@ -11,6 +12,7 @@ const app = express()
 //Connect to database
 connectDB()
 
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
