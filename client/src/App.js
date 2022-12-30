5376d8bc9dc5a902a175da22fd0c03934998c369
Michael Chop <007micle007@gmail.com>
2022-12-30 23:17:14 +0300

    Optimized the functionality for deleting clients using the Apollo Cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from "./components/Header";
 import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
 import Clients from "./components/Clients";
 
+const fixedCache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming
+                    }
+                },
+                projects: {
+                    merge(existing, incoming) {
+                        return incoming
+                    }
+                }
+            }
+        }
+    }
+})
+
 const client = new ApolloClient({
     uri: 'http://localhost:5000/graphql',
-    cache: new InMemoryCache()
+    cache: fixedCache
 })
 
 function App() {
