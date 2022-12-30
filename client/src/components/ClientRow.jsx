a3da2c7c519d057f7dd6773255af2d1f12e34541
Michael Chop <007micle007@gmail.com>
2022-12-30 22:50:18 +0300

    Added delete client mutation

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {FaTrash} from "react-icons/fa";
+import {useMutation} from "@apollo/client";
+import {DELETE_CLIENT} from "../mutations/client-mutations";
 
 const ClientRow = ({client}) => {
+    const [deleteClient] = useMutation(DELETE_CLIENT, {
+        variables: { id: client.id }
+    })
+
     return (
         <tr>
             <td>{client.name}</td>
             <td>{client.email}</td>
             <td>{client.phone}</td>
             <td>
-                <button className="btn btn-danger btn-sm">
-                    <FaTrash />
+                <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+                    <FaTrash/>
                 </button>
             </td>
         </tr>
---
+++ client/src/mutations/client-mutations.js
@@ -0,0 +1,9 @@
+import {gql} from "@apollo/client";
+
+export const DELETE_CLIENT = gql`
+    mutation deleteClient($id: ID!) {
+        deleteClient(id: $id) {
+            id, name, email, phone
+        }
+    }
+`
